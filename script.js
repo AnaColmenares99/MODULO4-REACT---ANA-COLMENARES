d5d49b980765f0b16161b480aacb1a852e104f43
Ana María Colmenares <anamariacolmenares1@gmail.com>
2024-11-10 07:06:53 -0500

    Tarea 1 - Modulo 4

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// Crear un elemento 'div'
+const div = document.createElement('div');
+
+// Agregar un atributo 'id' al elemento div que acabo de crear
+div.id = 'miDiv';
+
+// Agregar texto al 'div'
+div.textContent = 'Este es un ejemplo de un div creado con JavaScript';
+
+// Cambiar el color de fondo del 'div'
+div.style.backgroundColor = "yellow";
+
+// Agregar el 'div' al cuerpo del documento
+document.body.appendChild(div);
+
